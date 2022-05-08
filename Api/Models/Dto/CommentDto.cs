b4f06310ea385e544f238af947f158ad8fa60f47
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-05-08 12:30:07 +0200

    dtos: remove constructors from comment and tag

--- Api/Models/Dto/CommentDto.cs
+++ Api/Models/Dto/CommentDto.cs
@@ -2,7 +2,6 @@ namespace Api.Models
 {
     public class CommentDto
     {
-        public CommentDto() { }
         public int Id { get; set; }
         public string Author { get; set; }
         public string Content { get; set; }
--- Api/Models/Dto/TagDto.cs
+++ Api/Models/Dto/TagDto.cs
@@ -2,8 +2,6 @@ namespace Api.Models
 {
     public class TagDto
     {
-        public TagDto() { }
-
         public int Id { get; set; }
         public string Name { get; set; }
     }
