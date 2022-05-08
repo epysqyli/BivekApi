f8e534db088ae77f819b56e9abf746355c8adbd7
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-05-08 17:41:26 +0200

    test: add incomplete controller test

---
+++ Api.Tests/Controllers/ArticlesControllerTest.cs
@@ -0,0 +1,19 @@
+using System;
+using Moq;
+using Xunit;
+using System.Threading.Tasks;
+using Api.Models;
+using Api.Data;
+using Api.Controllers;
+
+namespace Api.UnitTests.Controllers
+{
+    public class ArticlesControllerTest
+    {
+        [Fact]
+        public async Task Create_ReturnsBadRequest_GivenInvalidModel()
+        {
+
+        }
+    }
+}
