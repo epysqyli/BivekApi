15c29f7942a1b990bc8bdfc3be6f1a4fb894c3f5
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-05-10 23:21:48 +0200

    test: add basic article controller test

--- Api.Tests/Controllers/ArticlesControllerTest.cs
+++ Api.Tests/Controllers/ArticlesControllerTest.cs
@@ -2,7 +2,10 @@ using System;
 using Moq;
 using Xunit;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
 using Api.Models;
+using Api.Interfaces;
 using Api.Data;
 using Api.Controllers;
 
@@ -11,9 +14,30 @@ namespace Api.UnitTests.Controllers
     public class ArticlesControllerTest
     {
         [Fact]
-        public async Task Create_ReturnsBadRequest_GivenInvalidModel()
+        public void GetArticle_Returns_HttpNotFound_GivenNotexistentArticle()
         {
+            Moq.Mock<IUnitOfWork> mockIUnitOfWork = new Mock<IUnitOfWork>();
+            int fakeArticleId = 123;
+            mockIUnitOfWork.Setup(unit => unit.Articles.GetById(fakeArticleId)).Returns<Article>(null);
+            ArticlesController articlesController = new ArticlesController(mockIUnitOfWork.Object);
 
+            IActionResult res = articlesController.GetArticle(fakeArticleId);
+
+            Assert.IsType<NotFoundResult>(res);
+        }
+
+        [Fact]
+        public void Create_ReturnsBadRequest_GivenInvalidModel()
+        {
+            Moq.Mock<IUnitOfWork> mockIUnitOfWork = new Mock<IUnitOfWork>();
+            IUnitOfWork mock = mockIUnitOfWork.Object;
+            ArticlesController articlesController = new ArticlesController(mock);
+            articlesController.ModelState.AddModelError("error", "generic error");
+
+            Article article = new Article() { Title = null };
+            IActionResult res = articlesController.CreateArticle(article);
+
+            Assert.IsType<BadRequestObjectResult>(res);
         }
     }
 }
