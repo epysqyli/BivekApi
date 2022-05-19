589003b3bb2cdd501ca9e0c2ec4b68ae940af3a8
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-05-19 21:13:38 +0200

    migrations: add title to datasets

--- Api/Models/Entities/Article.cs
+++ Api/Models/Entities/Article.cs
@@ -16,13 +16,6 @@ namespace Api.Models
         public ICollection<Comment> Comments { get; set; }
         public ICollection<ArticleTag> ArticleTags { get; set; }
 
-        public void AddComment(Comment comment, ApiDbContext context)
-        {
-            comment.Article = this;
-            context.Comments.Add(comment);
-            context.SaveChanges();
-        }
-
         public override string ToString()
         {
             return $"Title: {this.Title}\nBody: {this.Body}\n";
