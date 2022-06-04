8e57b999d62685a7d8fe90b90e3d5c93743f8074
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-06-04 12:59:48 +0200

    article: define full dto interface

--- Api/Interfaces/IArticleDto.cs
+++ Api/Interfaces/IArticleDto.cs
@@ -1,7 +1,19 @@
+using Api.Models;
+
 namespace Api.Interfaces
 {
     public interface IArticleDto
     {
+        int Id { get; set; }
+        string Title { get; set; }
+        string Body { get; set; }
+
+        List<TagDto> Tags { get; set; }
+        List<CommentDto> Comments { get; set; }
+
+        DateTime? CreatedAt { get; set; }
+        DateTime? UpdatedAt { get; set; }
+        bool Published { get; set; }
         bool isNull();
     }
 }
