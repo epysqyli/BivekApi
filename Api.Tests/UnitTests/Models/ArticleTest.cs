fc52fbe17f3fea28e62ca01e8f84d3e36dd50ea4
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-07-09 15:34:40 +0200

    tests: restore article test

--- Api.Tests/UnitTests/Models/ArticleTest.cs
+++ Api.Tests/UnitTests/Models/ArticleTest.cs
@@ -9,32 +9,16 @@ namespace Api.UnitTests.Models
     public class ArticleTest
     {
         [Fact]
-        public void TitleIsAssigned()
-        {
-            Article article = new Article()
-            {
-                Title = "Test Title",
-                Body = "Test body for the article"
-            };
-
-            Assert.Same("Test Title", article.Title);
-        }
-
-        [Fact(Skip = "needs integration testing, validation is triggered at runtime")]
-        public void ArticleWithDuplicateTitleIsNotCreated()
+        public void ArticleIsCreated()
         {
             DbContextOptions<ApiDbContext> options = new DbContextOptionsBuilder<ApiDbContext>()
                                                          .UseInMemoryDatabase(databaseName: "BlogDatabase").Options;
 
-            string title = "New Article Title";
-            Article firstArticle = new Article() { Title = title, Body = "some content" };
-            Article secondArticle = new Article() { Title = title, Body = "some other content" };
+            Article article = new Article() { Title = "New Article Title", Body = "some content" };
 
             using (ApiDbContext context = new ApiDbContext(options))
             {
-                context.Articles.Add(firstArticle);
-                context.SaveChanges();
-                context.Articles.Add(secondArticle);
+                context.Articles.Add(article);
                 context.SaveChanges();
                 Assert.Equal(1, context.Articles.Count());
             }
