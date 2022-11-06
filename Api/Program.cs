2b1ab1038ffaa88de16c389f0efea910cdd19a7f
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-11-06 22:14:58 +0100

    settings: add ENV variable and allow openapi.

--- Api/Program.cs
+++ Api/Program.cs
@@ -62,12 +62,8 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseRouting();
 app.UseCors(CorsPolicy);
